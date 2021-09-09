2c131a4af916f59eae4d784d67f4360b05b3eab1
unknown <mm.ashjaei@gmail.com>
2021-09-09 11:59:21 +0430

    Changes to be committed:     my First Virsion

---
+++ requestWeather.py
@@ -0,0 +1,24 @@
+import requests 
+import json
+import time
+
+def reqF (loc):
+    for count in range (2):
+        MyUrl = "https://api.openweathermap.org/data/2.5/weather"
+        MyParams = {'q' : str(location) , 'appid' : 'fe8a34a39bab345ee66884db604a92a5'}
+        response = requests.get(url= MyUrl, params= MyParams ,timeout=3.05)
+        data =json.loads(response.text)
+        #print(data)
+        print ("---------{}---------".format(count))
+        print("temp :      {}".format(data['main']['temp']))
+        print("humidity :  {}".format(data['main']['humidity']))
+        print("time :      {}".format(time.ctime(int(data['dt']))))
+        time.sleep(5)
+
+while True :
+    location = input('Enter Your City (for exit type end)...: ')
+    if location == 'end' :
+        break
+    print("Location is {}".format(location))
+    reqF(location)
+        
